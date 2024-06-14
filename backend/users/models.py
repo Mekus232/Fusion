9eb304b4d66eb0f8d10e30d3414afd47cec16fd4
johnpauljpc <jpcwork081@gmail.com>
2024-06-14 15:11:13 +0100

    custom user model created

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+ROLE = (
+    ('student', 'Student'),
+    ('teacher', 'Teacher')
+)
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=50, null= False, blank= False)
+    last_name = models.CharField(max_length=50, null= False, blank= False)
+    role = models.CharField(max_length=50, choices = ROLE)
+
+    def __str__(self):
+        return self.username
+    
